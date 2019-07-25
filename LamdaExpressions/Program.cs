454788a1cb174271b7d4e7ce95a699d61e551c63
Billy Tukija Richard <billytukija@gmail.com>
2019-07-25 09:45:33 -0300

    LambdaExpressions
    Adding others options to work with lambda expression

--- LamdaExpressions/Program.cs
+++ LamdaExpressions/Program.cs
@@ -10,14 +10,43 @@ namespace LamdaExpressions
     {
         static void Main(string[] args)
         {
-            //1) All courses
-            var couse = new List<Course>() {
+            // All courses
+            var courses = new List<Course>() {
                 new Course{ Id=1, Canal= Canal.DotNet, Hours=24, PublishedDate = DateTime.Now,Title="Test"},
                 new Course{ Id=1, Canal= Canal.DotNet, Hours=30, PublishedDate = DateTime.Now,Title="Test2"}
             };
 
-            //2) ORDER BY Publication date
-            var course = couse.OrderBy(x => x.PublishedDate);
+            //ORDER BY Publication date
+            var courses = courses.OrderBy(x => x.PublishedDate);
+
+            // ORDER BY DataPublicacao, CargaHoraria
+            //var courses = db.courses.OrderBy(c => c.PublishedDate).ThenBy(c => c.Hours);
+
+            // ORDER BY DataPublicacao, CargaHoraria DESC
+            //var courses = db.courses.OrderBy(c => c.PublishedDate).ThenByDescending(c => c.Hours);
+
+            //WHERE Canal = Python
+            //var courses = db.courses.Where(c => c.Canal == Canal.Python);
+
+            //WHERE CargaHoraria
+            //var courses = db.courses.Where(c => c.Hours > 20);
+
+            //TOP 5 + ORDER BY DATA DESC
+            //var courses = db.courses.OrderByDescending(c => c.PublishedDate).Take(5);            
+
+            //foreach (Course c in courses)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+            //TOP 1 + ORDER BY DATA DESC
+            //var courses = db.courses.OrderByDescending(c => c.PublishedDate).ThenByDescending(c => c.Id).FirstOrDefault();
+            //Console.WriteLine(curso);
+
+            // COUNT(*) WHERE CANAL = Engenharia de Software
+            //int total = db.Course.Count(c => c.Canal == Canal.EngenhariaDeSoftware);
+            //Console.WriteLine(total);
+
 
         }
     }
